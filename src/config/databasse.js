f24cf55dc1eb4e8c6fbcb03cf1e5d68b58c8a27b
Wellici Araujo <wja1@ifal.edu.br>
2019-10-19 13:39:39 -0300

    configuração do sequelize

---
+++ src/config/databasse.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'gympoint123',
+  database: 'gympoint',
+  define: {
+    timestemps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
