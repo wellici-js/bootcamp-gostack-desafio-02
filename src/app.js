fa8242a935a4ae5f0a6857df4bc7b00331959005
Wellici Araujo <wja1@ifal.edu.br>
2019-10-19 21:33:25 -0300

    eslint prettier

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 import express from 'express';
+// import bodyParser from 'body-parser';
+import logs from './app/middlewares/Logs';
+import routes from './routes';
 
 class App {
   constructor() {
     this.server = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+    this.server.use(logs);
+    // this.server.use(bodyParser());
+  }
+
+  routes() {
+    this.server.use(routes);
   }
 }
 
---
+++ src/app/middlewares/Logs.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+export default (req, res, next) => {
+  // eslint-disable-next-line no-console
+  console.log('ola mundo');
+
+  next();
+};
